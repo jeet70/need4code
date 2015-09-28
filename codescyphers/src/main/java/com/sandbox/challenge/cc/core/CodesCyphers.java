757318be4a739e7239b11bb83cfff9d7e6dfe15e
jeet70 <sharansinghjeet@gmail.com>
2015-09-28 16:19:22 +0530

    updated code

--- codescyphers/src/main/java/com/sandbox/challenge/cc/core/CodesCyphers.java
+++ codescyphers/src/main/java/com/sandbox/challenge/cc/core/CodesCyphers.java
@@ -1,129 +1,166 @@
-package com.sandbox.challenge.cc.core;
+package com.sj.sample.hello.sample_hello;
 
 import java.util.Scanner;
 
+/**
+ * 
+ * @author Team sandboX
+ *
+ */
 public class CodesCyphers {
 
 	public static void main(String[] args) {
 		
-		String msg = "SharanSingh";
-		String key ="jeet";
-		Scanner scan;
-		
-		char[] msgCase = msg.toCharArray();
-		int[] caseCode = new int[msgCase.length];
-		char[] msgArray =msg.toLowerCase().toCharArray();
-		char[] keyArray =key.toLowerCase().toCharArray();
-		int[] msgCode = new int[msgArray.length] ;
-		int[] keyCode =new int[msgArray.length];
-		int[] cipherCode = new int[msgArray.length];
-		char[] cipherArray = new char[msgArray.length];
-		int[] decipherCode = new int[cipherArray.length];
-		char[] decipherArray = new char[cipherArray.length];
-		int counter = 0;
-		
-		System.out.println("");
-		
-		/*for(int i =0;i<msgCase.length;i++)
-		{
-			if(((int)msgCase[i])>64 && ((int)msgCase[i])< 91)
-			{
-				caseCode[i]=1;
-			}
-		}*/
-		
-		for(int i=0;i<msgArray.length;i++)
-		{
-			if(((int)msgArray[i])>95 && ((int)msgArray[i])<123)
-				msgCode[i]=((int)msgArray[i])-96;
-			else
-				msgCode[i]=((int)msgArray[i]);
-			System.out.print(msgCode[i]+" ");
-		}
-		
-		System.out.println();
-		
-		for(int i=0;i<msgArray.length&&counter<msgArray.length;i++)
-		{
-			if(((int)msgArray[counter])>95 && ((int)msgArray[counter])<123)
-			{
-				keyCode[counter]=((int)keyArray[i])-96;
-				System.out.print(keyCode[counter]+" ");
-			}
-			else
-			{
-				keyCode[counter]=((int)msgArray[counter]);
-				System.out.print(keyCode[counter]+" ");
-				i=i-1;
-			}
-			if(i==keyArray.length-1)
-			{
-				i=-1;
-			}
-				counter++;
-				
-		}
-		
-		System.out.println();
-		
-		for(int i=0;i<msgArray.length;i++)
-		{
-			if(((int)msgArray[i])>95 && ((int)msgArray[i])<123)
-			{
-				cipherCode[i]=(msgCode[i]+keyCode[i])%26+1;
-			}
-			else
-			{
-				cipherCode[i] = msgCode[i];
-			}
-			System.out.print(cipherCode[i]+" ");
-			
-		}
-		
-		System.out.println();
-		
-		for(int i=0;i<msgArray.length;i++)
-		{
-			if(((int)msgArray[i])>95 && ((int)msgArray[i])<123)
-			{
-				cipherArray[i]=(char)(cipherCode[i]+96);
-			}
-			else
-			{
-				cipherArray[i]=(char)(cipherCode[i]);
-			}
-			
-			System.out.print(cipherArray[i]+" ");
-			
-		}
-		
-		/*String cipherText = cipherArray.toString();
-		
-		for(int i =0;i<msgCase.length;i++)
-		{
-			if(caseCode[i]==1)
-			{
-				cipherText =Character.toUpperCase(i)+cipherText.substring(i+1);
-			}
-		}*/
-		
-		System.out.println();
-		
-		for(int i=0;i<msgArray.length;i++)
-		{
-			if(((int)cipherArray[i])>95 && ((int)cipherArray[i])<123)
-			{
-				decipherCode[i]=(cipherCode[i]-keyCode[i])>0?(cipherCode[i]-keyCode[i])%26-1:(cipherCode[i]-keyCode[i])+25;
-				decipherArray[i] =(char)(decipherCode[i]+96);
-			}
-			else
-			{
-				decipherCode[i]=cipherCode[i];
-				decipherArray[i]=(char)(decipherCode[i]);
-			}
-			System.out.print(decipherArray[i]);
-		}
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Key: ");
+		String key = scan.nextLine();
 
-	}
+		System.out.print("Is message plaintext (Y/N): ");
+		String selection = scan.nextLine();
+		char[] keyArray =key.toLowerCase().toCharArray();
+		
+		if(selection.equalsIgnoreCase("y"))
+		{
+			
+			System.out.print("Plaintext: ");
+			String msg = scan.nextLine();
+			char[] msgArray =msg.toLowerCase().toCharArray();
+			char[] caseArray = msg.toCharArray();
+			int[] caseCode = new int[caseArray.length];
+			int[] msgCode = new int[msgArray.length] ;
+			int[] keyCode =new int[msgArray.length];
+			int[] cipherCode = new int[msgArray.length];
+			char[] cipherArray = new char[msgArray.length];
+			
+			for(int i =0;i<caseArray.length;i++)
+    		{
+    			if(((int)caseArray[i])>64 && ((int)caseArray[i])< 91)
+    			{
+    				caseCode[i]=1;
+    			}
+    		}
+			
+			for(int i=0;i<msgArray.length;i++)
+			{
+				if(((int)msgArray[i])>95 && ((int)msgArray[i])<123)
+					msgCode[i]=((int)msgArray[i])-96;
+				else
+					msgCode[i]=((int)msgArray[i]);
+			}
+			
+			for(int i=0,counter = 0;i<msgArray.length&&counter<msgArray.length;i++,counter++)
+			{
+				if(((int)msgArray[counter])>95 && ((int)msgArray[counter])<123)
+				{
+					keyCode[counter]=((int)keyArray[i])-96;
+				}
+				else
+				{
+					keyCode[counter]=((int)msgArray[counter]);
+					i=i-1;
+				}
+				if(i==keyArray.length-1)
+				{
+					i=-1;
+				}
+					
+			}
+			
+			for(int i=0;i<msgArray.length;i++)
+			{
+				if(((int)msgArray[i])>95 && ((int)msgArray[i])<123)
+				{
+					cipherCode[i]=(msgCode[i]+keyCode[i])%26+1;
+					cipherCode[i]=(cipherCode[i]+96);
+					if(caseCode[i]==1)
+					{
+						cipherCode[i]=(cipherCode[i]-32);
+					}
+					cipherArray[i]=(char)(cipherCode[i]);
+				}
+				else
+				{
+					cipherCode[i] = msgCode[i];
+					cipherArray[i]=(char)(cipherCode[i]);
+				}
+			}
+			
+			String cipherText = new String(cipherArray);
+			System.out.println("Output message: "+cipherText);
+			
+		}
+		
+		if(selection.equalsIgnoreCase("n"))
+		{	
+			
+			System.out.print("Ciphertext: ");
+			String cipher = scan.nextLine();
+			char[] cipherArray = cipher.toLowerCase().toCharArray();
+			int[] cipherCode = new int[cipherArray.length];
+			char[] caseArray = cipher.toCharArray();
+			int[] caseCode = new int[caseArray.length];
+			int[] keyCode =new int[cipherArray.length];
+			int[] decipherCode = new int[cipherArray.length];
+			char[] decipherArray = new char[cipherArray.length];
+			
+			for(int i =0;i<caseArray.length;i++)
+    		{
+    			if(((int)caseArray[i])>64 && ((int)caseArray[i])< 91)
+    			{
+    				caseCode[i]=1;
+    			}
+    		}
+			
+			for(int i=0;i<cipherArray.length;i++)
+			{
+				if(((int)cipherArray[i])>95 && ((int)cipherArray[i])<123)
+					cipherCode[i]=((int)cipherArray[i])-96;
+				else
+					cipherCode[i]=((int)cipherArray[i]);
+			}
+	
+			for(int i=0,counter = 0;i<cipherArray.length&&counter<cipherArray.length;i++,counter++)
+			{
+				if(((int)cipherArray[counter])>95 && ((int)cipherArray[counter])<123)
+				{
+					keyCode[counter]=((int)keyArray[i])-96;
+				}
+				else
+				{
+					keyCode[counter]=((int)cipherArray[counter]);
+					i=i-1;
+				}
+				if(i==keyArray.length-1)
+				{
+					i=-1;
+				}
+					
+			}
+			
+			for(int i=0;i<cipherArray.length;i++)
+			{
+				if(((int)cipherArray[i])>95 && ((int)cipherArray[i])<123)
+				{
+					decipherCode[i]=(cipherCode[i]-keyCode[i])>0?(cipherCode[i]-keyCode[i])%26-1:(cipherCode[i]-keyCode[i])+25;
+					decipherCode[i]=(decipherCode[i]+96);
+					if(caseCode[i]==1)
+					{
+						decipherCode[i]=(decipherCode[i]-32);
+					}
+					decipherArray[i]=(char)(decipherCode[i]);
+				}
+				else
+				{
+					decipherCode[i]=cipherCode[i];
+					decipherArray[i]=(char)(decipherCode[i]);
+				}
+			}
+			
+			String decipherText = new String(decipherArray);
+			System.out.println("Output message: "+decipherText);
+	
+			}
+		
+		}
 
 }
